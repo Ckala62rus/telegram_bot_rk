966d8405df3682248b8e4dbf2553210e0622a719
Ckala <agr.akyla@mail.ru>
2025-01-16 16:05:54 +0300

    ( refactor ) edit main menu list.

--- src/common/bot_cmds_list.py
+++ src/common/bot_cmds_list.py
@@ -2,14 +2,8 @@ from aiogram.types import BotCommand
 
 
 private = [
-    BotCommand(command='looking_for', description="Поиск партии"),
-    BotCommand(command='paginate', description="Пагинация"),
-    BotCommand(command='categories', description="Категории"),
-    BotCommand(command='categories_init', description="Создать категории"),
     BotCommand(command='menu', description="Посмотреть меню"),
-    BotCommand(command='about', description="О нас"),
-    BotCommand(command='payment', description="Варианты оплаты"),
-    BotCommand(command='shipping', description="Варианты доставки"),
+    BotCommand(command='looking_for', description="Поиск партии"),
     BotCommand(command='phone', description="Поделиться телефоном"),
     BotCommand(command='about_me', description="Информация обо мне"),
 ]
