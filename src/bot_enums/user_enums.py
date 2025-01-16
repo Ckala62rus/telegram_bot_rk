5fd518553152d74abcbbfa7285ff2424eeaea30e
Ckala <agr.akyla@mail.ru>
2025-01-16 10:48:08 +0300

    ( refactor ) delete other garbage from admin handlers and user private handlers.

---
+++ src/bot_enums/user_enums.py
@@ -0,0 +1,20 @@
+from enum import StrEnum
+
+
+class FindPartyTypes(StrEnum):
+    SIMPLE = "искать партию",
+    WITH_COLOR = "искать партию с цветом",
+    WITH_FIO = "искать с фио",
+    CANCEL = "отмена",
+    STEP_BACK = "шаг назад"
+
+
+class FindPartyText(StrEnum):
+    CHOOSE_YEAR = "Выберете год или введите две цифры нужного"
+    SEND_YOUR_PHONE = "Отправьте свой номер телефона для регистрации"
+    ACTIONS_CANCELED = "Действия отменены"
+    PLEASE_ENTER_PART_NUMBER = "Введите номер партии для поиска"
+    ADDITION_INFORMATION = ("Если нужна дополнительная информация о партии, "
+                            "выберете соответствующую кнопку "
+                            "(запрос займет немного больше времени)?")
+    LOOKING_PARTY = "Поиск партии..."
